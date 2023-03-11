c22b9880a56381975545b87a176730b84cb0a688
Andriw-afonso <andriw_eng@outlook.com>
2023-03-11 09:13:08 -0300

    hnbbjvgvgjvgvjgvjhg

--- meu-projeto/src/App.js
+++ meu-projeto/src/App.js
@@ -3,6 +3,7 @@ import HelloWorld from './components/HelloWorld';
 import SayMyName from "./components/SayMyName"
 import Pessoa from "./components/Pessoa"
 import Frase from './components/Frase'
+import List from './components/List'
 
 function App() {
   const name = 'Mateus'
@@ -36,6 +37,7 @@ function App() {
      />
      <Frase/>
      <Frase/>
+     <List/>
     </div>
   );
 }
