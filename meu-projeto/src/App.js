d803f87f1d613a1b2d3de57a50f60979daf23a72
Andriw-afonso <andriw_eng@outlook.com>
2023-03-11 08:14:10 -0300

    hjbhbjhbjbjhbj

--- meu-projeto/src/App.js
+++ meu-projeto/src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import HelloWorld from './components/HelloWorld';
 import SayMyName from "./components/SayMyName"
 import Pessoa from "./components/Pessoa"
+import Frase from './components/Frase'
 
 function App() {
   const name = 'Mateus'
@@ -31,7 +32,10 @@ function App() {
         idade = "28"
         profissao = "Programador"
         foto = "https://via.placeholder.com/150"
+        
      />
+     <Frase/>
+     <Frase/>
     </div>
   );
 }
