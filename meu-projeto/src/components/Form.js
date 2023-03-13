3506a2be2daccb4a586e4542e88c1e2dbcd65f61
Andriw-afonso <andriw_eng@outlook.com>
2023-03-13 17:03:37 -0300

    bnkhbjkhbjkhb

--- meu-projeto/src/components/Form.js
+++ meu-projeto/src/components/Form.js
@@ -1,22 +1,40 @@
+import {useState} from 'react'
+
 function Form (){
     function cadastrarUsuario(e){
         e.preventDefault()
-        console.log('Cadastrou o usuário!')
+        console.log(`Usuário ${name} foi cadastrado com a senha: ${password} `)
     }
+
+    const [name, setName] = useState()
+    const [password, setPassword] = useState()
+
     return (
         <div>
-            <form onSubmit={cadastrarUsuario}>
-                <div>
-                    <label ></label>
-                    <input type="text" placeholder="Digite o seu nome"/>
-                </div>
-                <div>
-                    <input type="password" value="Digíte a sua senha."/>
-                </div>
-                <div>
-                    <input type="submit" value="Cadastro"/>
-                </div>
-            </form>
+            <h1>Meu cadastro: </h1>
+                <form onSubmit={cadastrarUsuario}>
+                    <div>  
+                        <label html="name ">Nome:</label>                      
+                        <input  
+                        type="text" 
+                        id="name"
+                        name="name"
+                        placeholder="Digite o seu nome"                       
+                        onChange={(e) => setName(e.target.value)}/>
+                    </div>
+                    <div>
+                        <label html="password">Senha:</label>                        
+                        <input 
+                        type="password" 
+                        id="password"
+                        name="password"
+                        placeholder="Digíte a sua senha."
+                        onChange={(e) => setPassword(e.target.value)} />
+                    </div>
+                    <div>
+                        <input type="submit" value="Cadastro"/>
+                    </div>
+                </form>
         </div>
     )
 }
