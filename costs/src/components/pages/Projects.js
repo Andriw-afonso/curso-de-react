56b57d1d3ba7dbfcd4c553c2b17139b8201182c3
Andriw-afonso <andriw_eng@outlook.com>
2023-03-24 16:00:59 -0300

    video30

--- costs/src/components/pages/Projects.js
+++ costs/src/components/pages/Projects.js
@@ -11,7 +11,7 @@ function Projects() {
 
   const [projects, setProjects] = useState([])
   const [removeLoading, setRemoveLoading] = useState(false)
-
+  const [projectMessage, setProjectMessage] = useState('')
   const location = useLocation()
 
   let message = ''
@@ -36,7 +36,23 @@ function Projects() {
         .catch((err) => console.log(err))
     },300)
   },[])
-  
+
+  function removeProject(id){
+
+    fetch(`http://localhost:5000/projects/${id}`,{
+      method:'DELETE',
+      headers:{
+        'Content-Type':'application/json'
+      },
+
+    }).then(resp => resp.json())
+    .then(() =>{
+        setProjects(projects.filter((project) => project.id !== id))
+        setProjectMessage('Projeto removido com sucesso')
+    })
+      .catch(err => console.log(err))
+  }
+    
   return (
     <div className={styles.project_container}>
 
@@ -46,6 +62,7 @@ function Projects() {
         </div> 
 
         {message && <Message type="success" msg={message} />}
+        {projectMessage && <Message type="success" msg={projectMessage} />}
 
         {projects.length > 0 &&
         projects.map((project)=>(
@@ -54,7 +71,8 @@ function Projects() {
           name={project.name}
           budget={project.budget}
           category={project.category.name}
-          key={project.id}          
+          key={project.id} 
+          handleRemove={removeProject}         
           />
         ))}
         {!removeLoading && <Loading/>}  
